96a70d28fac38fc34de0569b91fe2211298f2adb
Marco-Vigilio <tonyvigilio@gmail.com>
2023-07-05 15:52:58 +0200

    copiando uguale da registrazione

--- server.php
+++ server.php
@@ -2,9 +2,10 @@
 
 $string = file_get_contents("./db/dischi.json");
 
+//$result = json_encode($string);
+
 header('Content-Type: application/json');
 
-$result = json_encode($string);
 echo ($string);
 
 ?>
